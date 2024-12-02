020307982090bb601f6cc9cbf1c4c31149e2927a
Sinitsa <sinitsammi@gmail.com>
2024-12-02 14:37:06 +0300

    SwapString

---
+++ SwappingValues/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwappingValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            string name = "Синица";
+            string surname = "Евгений";
+            string swapValue;
+
+            Console.WriteLine($" Имя: {name}  Фамилия: {surname} ");
+
+            swapValue = name;
+            name = surname;
+            surname = swapValue;
+
+            Console.WriteLine($" Имя: {name}  Фамилия: {surname} ");
+        }
+    }
+}
